54c0ed3e3c40e004fb744422f78a6bc3d3240f75
jlearns9 <142127358+jlearns9@users.noreply.github.com>
2023-09-25 19:55:40 -0500

    uPdate

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import { spongeBobMemeData } from "/data.js"
 
 const spongeBobRadios = document.getElementById("spongeBobRadios")
+const getImgBtn = document.getElementById("getImgBtn")
+const gifsOnlyOption = document.getElementById("gifs-only-option")
+const memeModalInner = document.getElementById("meme-modal-inner")
+const memeModal = document.getElementById("meme-modal")
+const closeMemeModalBtn = document.getElementById("close-meme-modal-btn")
+
 const testBtn = document.getElementById("testBtn")
 
+
+spongeBobRadios.addEventListener('change', highlightSelectedEmotion)
+getImgBtn.addEventListener('click', renderMeme)
 
 
 
@@ -25,12 +34,12 @@ function renderEmotionsRadios(memes) {
     const emotions = getEmotionsArray(memes)
     for (let emotion of emotions) {
         radioItems += `
-        <div>
+        <div class="radio">
             <label for="${emotion}">${emotion}</label>
             <input
             type="radio"
             id="${emotion}"
-            value=${emotion}"
+            value="${emotion}"
             name="emotions"
             >
         </div>
@@ -39,14 +48,66 @@ function renderEmotionsRadios(memes) {
     spongeBobRadios.innerHTML = radioItems
 }
 
-
-
 renderEmotionsRadios(spongeBobMemeData)
 
+// Step 3 - Highlight the selected emotion, remove highlight if different emotion is selected
+function highlightSelectedEmotion(e) {
+    const radios = document.getElementsByClassName("radio")
+    for (let radio of radios) {
+        radio.classList.remove("highlight")
+    }
+    document.getElementById(e.target.id).parentElement.classList.add("highlight")
+}
+
+// Step 4 - When the get image button is pressed, it displays a meme
+function renderMeme() {
+    const memeObject = getSingleMemeObject()
+    memeModalInner.innerHTML = `
+        <img
+        class="meme-img"
+        src="./images/${memeObject.image}"
+        alt="${memeObject.alt}"
+        >
+        `
+    memeModal.style.display = "flex"
+}
+
+function getSingleMemeObject() {
+    const memesArray = getMatchingMemesArray()
+
+    if (memesArray.length === 1) {
+        return memesArray[0]
+    } else {
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        return memesArray[randomNumber]
+    }
+}
+
+function getMatchingMemesArray() {
+    if(document.querySelector('input[type="radio"]:checked')){
+        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+        const isGif = gifsOnlyOption.checked
+
+        const matchingMemesArray = spongeBobMemeData.filter(function(meme) {
+            if (isGif) {
+                return meme.emotionTags.includes(selectedEmotion) && meme.isGif
+            }
+            else{
+                return meme.emotionTags.includes(selectedEmotion)
+            }
+        })
+        return matchingMemesArray
+    }
+}
+
+// Step 5 - When the X button is pressed, it closes the meme and starts over
+function closeMemeModal() {
+dfas
+}
 
 // Console Log Button Tester //
 testBtn.addEventListener("click", function() {
-    console.log(emotions)
+
 }
 )
 
